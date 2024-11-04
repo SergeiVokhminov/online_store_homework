443adbbc601166f87af649d27bd69659bbc005a7
sergei_vokhminov <rreaper@list.ru>
2024-11-04 21:01:29 +0300

    добавлены файлы проекта

--- catalog/models.py
+++ catalog/models.py
@@ -2,10 +2,8 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=150, verbose_name='Название', help_text='Введите название', blank=True,
-                            null=True)
-    description = models.TextField(max_length=250, verbose_name='Описание', help_text='Введите описание', blank=True,
-                                   null=True)
+    name = models.CharField(max_length=100, verbose_name='Категория', help_text='Введите категорию товара')
+    description = models.TextField(verbose_name='Описание категории', help_text='Введите описание категории')
 
     def __str__(self):
         return f"{self.name}"
@@ -17,16 +15,15 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=100, verbose_name='Название', help_text='Введите название')
-    description = models.TextField(max_length=250, verbose_name='Описание', help_text='Введите описание')
-    image = models.ImageField(upload_to='media/photo', blank=True, null=True, verbose_name='Фото',
-                              help_text='Загрузите фотографию')
-    category = models.ForeignKey("Category", on_delete=models.CASCADE, verbose_name='Категория',
-                                 help_text='Введите категорию', blank=True, null=True)
-    price = models.IntegerField(verbose_name='Цена за покупку', help_text='Введите цену', blank=True, null=True)
-    created_at = models.DateTimeField(verbose_name='Дата создания', help_text='Введите дату создания', blank=True,
-                                      auto_now_add=True, null=True)
-    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения', blank=True, null=True)
+    name = models.CharField(max_length=100, verbose_name='Товар', help_text='Введите название товара')
+    description = models.TextField(verbose_name='Описание товара', help_text='Введите описание товара')
+    image = models.ImageField(upload_to='media/photo', blank=True, null=True, verbose_name='Изображение товара',
+                              help_text='Загрузите изображение товара')
+    category = models.ForeignKey("Category", on_delete=models.SET_NULL, verbose_name='Категория',
+                                 help_text='Введите категорию', related_name="products", blank=True, null=True)
+    price = models.FloatField(verbose_name='Цена товара', help_text='Введите цену товара')
+    created_at = models.DateField(verbose_name='Дата создания', auto_now_add=True, blank=True, null=True)
+    updated_at = models.DateField(verbose_name='Дата последнего изменения', auto_now=True, blank=True, null=True)
 
     def __str__(self):
         return f"{self.name}"
