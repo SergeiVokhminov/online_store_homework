7f05bca09717c6dd0844823919f13dc573053d21
sergei_vokhminov <rreaper@list.ru>
2024-12-17 23:26:30 +0300

    обновлены модули приложения users

--- users/views.py
+++ users/views.py
@@ -1,15 +1,15 @@
 from django.contrib.auth import login
 from django.core.mail import send_mail
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic.edit import CreateView
 
 from config import settings
 from users.forms import UserRegisterForm
-from users.models import User
+# from users.models import User
 
 
 class UserRegisterView(CreateView):
-    model = User
+    # model = User
     form_class = UserRegisterForm
     template_name = 'users/register.html'
     success_url = reverse_lazy("users:login")
@@ -23,6 +23,6 @@ class UserRegisterView(CreateView):
     def send_welcome_mail(self, user_email):
         subject = "Добро пожаловать на наш сайт"
         message = "Спасибо, что зарегистрировались на нашем сайте MoS!"
-        from_email = settings.DEFAULT_FROM_EMAIL
-        recipient_list = [user_email,]
+        from_email = settings.EMAIL_HOST_USER
+        recipient_list = [user_email]
         send_mail(subject, message, from_email, recipient_list)
