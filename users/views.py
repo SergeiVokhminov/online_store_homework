545882a840509013af11106ccb608c3dcfa142a3
sergei_vokhminov <rreaper@list.ru>
2024-12-20 18:51:43 +0300

    обновлены модуль views приложения users

--- users/views.py
+++ users/views.py
@@ -1,17 +1,17 @@
 from django.contrib.auth import login
+from django.conf import settings
 from django.core.mail import send_mail
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 
-from config import settings
 from users.forms import UserRegisterForm
-# from users.models import User
+from users.models import User
 
 
 class UserRegisterView(CreateView):
-    # model = User
+    model = User
     form_class = UserRegisterForm
-    template_name = 'users/register.html'
+    template_name = "users/register.html"
     success_url = reverse_lazy("users:login")
 
     def form_valid(self, form):
