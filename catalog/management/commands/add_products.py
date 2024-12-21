d27ad6bca5c4754c52c4af9e884e7330530e8b92
sergei_vokhminov <rreaper@list.ru>
2024-12-21 22:24:53 +0300

    обновлен модуль add_products

--- catalog/management/commands/add_products.py
+++ catalog/management/commands/add_products.py
@@ -7,9 +7,9 @@ from catalog.models import Category, Product
 class Command(BaseCommand):
     help = "Загружает данные в БД из fixture.json, предварительно очистив БД."
 
-    def handle(self, *args, **kwargs):
+    def handle(self, *args, **options):
         Category.objects.all().delete()
         Product.objects.all().delete()
 
-        call_command("loaddata", "catalog_fixture.json")
+        call_command("loaddata", "fixture/catalog_fixture.json")
         self.stdout.write(self.style.SUCCESS("БД успешно заполнена данными из fixture"))
